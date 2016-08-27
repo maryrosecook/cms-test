42079a9389078380564477fe1b188ff57a1560dd
Sachin Karia <sachin.karia01@gmail.com>
2016-08-27 16:38:22 +0100

    add text block service function completed

--- controller.js
+++ controller.js
@@ -1,3 +1,8 @@
 app.controller('controller', ['$scope','blockService',function($scope, blockService){
   $scope.test = blockService.test;
+  $scope.finalArticle = blockService.finalArticle;
+
+  $scope.addTextBlock = function(block){
+    blockService.addTextBlock($scope.bodyText,$scope.titleText);
+  };
  }]);
